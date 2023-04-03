be00bd6f1c6510024e2fed193747e389ebd9fde5
Aldo Berziu <berziu2004@gmail.com>
2023-04-03 10:07:25 +0200

    added cors

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const cookieParser = require("cookie-parser");
 const path = require('path');
+const cors = require('cors');
 const AppError = require('./utils/appError');
 
 const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+    origin: '*',
+  })
+);
+
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+
+app.use(allowCrossDomain);
+
 
 app.use(cookieParser());
 
